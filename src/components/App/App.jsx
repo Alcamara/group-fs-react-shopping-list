88099d6be6df1aa1145a63bf076e0fc119897231
Stephen Womack <swomack111@gmail.com>
2022-06-09 10:55:55 -0500

    List item in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
+import ListItem from '../ListItem/ListItem'
+
 
 function App() {
     return (
@@ -10,6 +12,14 @@ function App() {
             <Header />
             <main>
                 <p>Under Construction...</p>
+
+                
+                <div>
+                    {groceryItems.map(item =>
+                        <ListItem item={item}/>
+                    )}
+                    
+                </div>
             </main>
         </div>
     );
