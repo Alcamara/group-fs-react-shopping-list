17e08f2067d49534bafb8a7860ecedca7b63a5d3
Al Camara <Alhousseinec@gmail.com>
2022-06-09 10:08:23 -0500

    added database and values

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "list" (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR(80) NOT NULL,
+"quantity" INTEGER NOT NULL,
+"unity" VARCHAR(20),
+"purchaseStatus" BOOLEAN
+
+);
+
+INSERT INTO "list" ( "name","quantity", "unity", "purchaseStatus")
+VALUES ('Apples', 5,'lbs',false), ('Bread',1,'loaf',false), ('Milk',1,'gal',false)
