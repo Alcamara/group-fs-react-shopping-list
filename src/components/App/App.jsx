f9fa64dc2126eab24f5a663a7e248b0e3870f79b
Al Camara <Alhousseinec@gmail.com>
2022-06-09 10:51:17 -0500

    created get axios request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-
+//added useEffect and useState
+import { useEffect, useState } from 'react';
+//import axios
+import axios from 'axios'
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+    //created state
+    const [groceryItems,setGroceryItems] = useState([]);
+
+    function getGroceryItems(){
+
+        axios({
+            url:'/groceries',
+            method:'GET',
+        }).then((results)=>{
+            console.log('Get',results.data);
+            
+        }).catch((err)=>{
+            console.log('Get require failed',err)
+        })
+    }
+
     return (
         <div className="App">
             <Header />
